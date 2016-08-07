0d1e2c475906814369aa6cfb47cfa15bb9279831
myronliu <myron@liuyongpengdeMacBook-Pro.local>
2016-08-07 12:21:46 +0800

    去掉继续按钮

--- app/helper/total.js
+++ app/helper/total.js
@@ -3,7 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
   render: function(){
     return (
-      <div className={this.props.show ? "totalDialog" : "display"}>
+      <div className={this.props.show ? "totalDialog" : "display"} onTouchEnd={this.props.onTouchEnd}>
         <div className="total-boxMain">
           <img src="/images/win.png" />
           <span>100金币</span>
@@ -11,9 +11,7 @@ module.exports = React.createClass({
         <div className="total-kuang">
           恭喜你完成所有关卡
         </div>
-        <div className="total-reward-ok">
-          <img onTouchEnd={this.props.onTouchEnd} src="/images/goon.png" />
-        </div>
+        
       </div>
     )
   }
